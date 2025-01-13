62f22f4d5824c1f461e65eb25e064a4c2f337ad9
Lucas Alves <64313623+Lucas7Alves@users.noreply.github.com>
2025-01-13 15:15:09 -0300

    Start backend

---
+++ src/Back-end/demo/demo/src/main/java/com/bjfrios/demo/resources/UserResources.java
@@ -0,0 +1,20 @@
+package com.bjfrios.demo.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bjfrios.demo.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User("Maria", 1L, "maria@gmail.com", "123");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
